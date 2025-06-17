c077f54c35b22cda3c9fb8fe626dba4a5ab9b82b
Abhay Dixit <Dixitshivam534@gmail.com>
2025-06-17 07:39:26 +0530

    fix

--- Routes/Admin/AdminRoute.js
+++ Routes/Admin/AdminRoute.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const AdminModel = require('../../Module/Admin/AdminModule');
 const router = express.Router();
 
 const {
@@ -10,6 +11,15 @@ const {
 
 // Customer Registration
 router.post('/register', register);
+
+router.get('/alluser',async(req,res)=>{
+ try {
+    const alluser = await AdminModel.find()
+    res.status(200).json(alluser)
+ } catch (error) {
+    res.status(400).json(error)
+ }
+})
 
 // Customer Login
 router.post('/login', login );
